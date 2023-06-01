c931eafaa2ef9af535836c145f93b9fd9d398bd5
zhangc <40272460+798903520@users.noreply.github.com>
2023-06-01 16:19:43 +0800

    base俄罗斯

--- src/js/tetris.js
+++ src/js/tetris.js
@@ -1,6 +1,12 @@
 export default class tetris {
-    constructor(type = this.randomNum(7)) {
+    length=null// 边长 需要根据传过来的数据计算位置
+    type=null//方块类型
+    arr = null//方块数组
+    height=null// 初始高度
+    width=null// 初始宽度
+    constructor(length = 10,type = this.randomNum(7)) {
         this.type = type ;
+        this.length = length;
         this.init7Type()
     }
     //随机数据
@@ -9,11 +15,16 @@ export default class tetris {
     }
     //x长度 y长度
     fillArr(x,y){
-       let data = new Array(x).fill([]).map(() => Array().fill(1))
+       let data = [];
+       for(let i=0;i<x;i++){
+        data.push([]);
+        for(let j=0;j<y;j++){
+            data[i][j] = 1;
+        }
+       }
         return data;
     }
     init7Type(){
-
         //第一层数组长度是列 第二层数组长度是行
         switch (this.type) {
             case 0: //o型
@@ -28,17 +39,30 @@ export default class tetris {
                 this.arr[2][1] = 0;
                 break;
             case 3: //Z型
-                this.arr = this.fillArr(1,4);
+                this.arr = this.fillArr(3,2);
+                this.arr[2][0] = 0;
+                this.arr[0][1] = 0;
                 break;
             case 4: //L型
-                this.arr = this.fillArr(1,4);
+                this.arr = this.fillArr(2,3);
+                this.arr[1][0] = 0;
+                this.arr[1][1] = 0;
                 break;
             case 5: //J型
-                this.arr = this.fillArr(1,4);
+                this.arr = this.fillArr(2,3);
+                this.arr[0][0] = 0;
+                this.arr[0][1] = 0;
                 break;
             case 6: //T型
-                this.arr = this.fillArr(1,4);
+                this.arr = this.fillArr(3,2);
+                this.arr[0][1] = 0;
+                this.arr[2][1] = 0;
                 break;
         }
+        this.height = 0 - this.arr[0].length*this.length;
+        this.width = 0 - this.arr.length*this.length;
+    }
+    moveBlock(){
+        this.height = this.height + this.length;
     }
 }
--- src/view/game/tetris.vue
+++ src/view/game/tetris.vue
@@ -1,27 +1,188 @@
 <template>
-
+  <div class="eluosibg">
+    <div class="leftArea">{{ isPlay }}</div>
+    <canvas id="canBG" width="360" height="600"></canvas>
+    <div class="rightArea"></div>
+    <!-- <img src="/src/assets/img/bg.png" alt=""> -->
+  </div>
 </template>
 
 <script setup>
+import { computed, onMounted, reactive, ref } from "vue";
 import tetrisBlock from "../../js/tetris";
 
 
-function getTData (x) {
+const whBoth = 30;//边长
+
+
+function getTData(x) {
   let num = x;
   let itemData = [];
-  function returndata (num){
+  function returndata(num) {
     itemData = [];
-    for(let i = 0;i<num;i++){
+    for (let i = 0; i < num; i++) {
       itemData.push(new tetrisBlock());
     }
     return itemData;
   }
-  return  returndata(num);
+  return returndata(num);
 }
-const funkuai = getTData(10);
-console.log('funkuai',funkuai)
+
+
+// 初始化背景数据
+const BGData = ref([]);
+
+
+/**
+  * 填充背景数组
+  */
+function fillBGArr(x = 12, y = 20) {
+  let data = [];
+  for (let i = 0; i < x; i++) {
+    data.push([]);
+    for (let j = 0; j < y; j++) {
+      data[i][j] = 0;
+    }
+  }
+  BGData.value = data;
+  drawBG();
+}
+
+
+// 全局画笔
+const ctx = ref({});
+
+
+/**
+  * 画出背景
+  */
+async function drawBG() {
+  let img1 = new Image();
+  img1.src = '/src/assets/img/bg.png';
+
+  img1.onload = function () {
+    for (let itemx = 0; itemx < BGData.value.length; itemx++) {
+      for (let itemy = 0; itemy < BGData.value[itemx].length; itemy++) {
+        // console.log(`x=${itemx},y=${itemy}`);
+
+        // 填充矩形
+        ctx.value.strokeRect(itemx * whBoth, itemy * whBoth, whBoth, whBoth);
+        // ctx.fillRect(itemx*whBoth+whBoth/2.6,itemy*whBoth+whBoth/2.6,whBoth/4,whBoth/4);
+
+        // 填充自定义图片
+        // ctx.drawImage(img1, itemx * whBoth, itemy * whBoth, whBoth, whBoth)
+      }
+    }
+  }
+
+}
+
+
+/**
+  * 获取画笔
+  */
+function getDraw() {
+  let can = document.getElementById('canBG');
+  ctx.value = can.getContext('2d');
+
+  // 注册事件
+  window.onkeydown = function (e) {
+    console.log('e', e.which);
+
+    switch (count.value) {
+      case null:
+        count.value = true;
+        gameStart();
+        break;
+      case true:
+        if(e.which == 32){
+          count.value = false;
+          gameStop()
+        }
+        break;
+      case false:
+      if(e.which == 32){
+          count.value = true;
+          gameContinue()
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+// 时间控制
+let timmer = null;
+// 当前方块
+const nowFK = reactive({});
+// 下一个方块
+const nextFK = reactive({});
+
+//四种状态
+function gameStart(){
+  nowFK.value = new tetrisBlock(whBoth,0);
+  nextFK.value = new tetrisBlock(whBoth);
+  timmer = setInterval(()=> {
+    nowFK.value.moveBlock();
+    console.log('nowFK',nowFK.value);
+  },1000);
+}
+function gameStop(){
+  clearInterval(timmer);
+  timmer = null;
+}
+function gameContinue(){
+  timmer = setInterval(()=> {
+    console.log('nowFK',nowFK,nowFK);
+  },1000);
+}
+function gameEnd(){}
+
+
+const count = ref(null);
+const isPlay = computed(() => {
+  switch (count.value) {
+    case null:
+      return '点击任意键开始';
+    case true:
+      return '游戏中'
+    case false:
+      return '暂停中'
+  }
+});
+/**
+  * 变化状态
+  */
+
+
+onMounted(() => {
+  getDraw();
+  fillBGArr();
+  
+});
 </script>
 
 <style scoped lang="less">
+.eluosibg {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 
+  .leftArea,
+  .rightArea {
+    flex: 1;
+  }
+
+  .leftArea {
+    // text
+  }
+}
+
+#canBG {
+  border: 1px solid black;
+  margin: 0 20px;
+}
 </style>
